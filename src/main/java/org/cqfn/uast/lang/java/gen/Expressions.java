2f6f5ddc4911c1257b4fb15cb22ce383b23cfdc0
Ivan Kniazkov <ivan@kniazkov.com>
2022-09-29 10:32:35 +0300

    Generator for Java expressions

---
+++ src/main/java/org/cqfn/uast/lang/java/gen/Expressions.java
@@ -0,0 +1,92 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Ivan Kniazkov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cqfn.uast.lang.java.gen;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+import org.cqfn.astranaut.core.Node;
+import org.cqfn.uast.tree.green.Addition;
+import org.cqfn.uast.tree.green.Expression;
+
+/**
+ * Code generator for Java expressions.
+ *
+ * @since 0.1.2
+ */
+public class Expressions {
+    /**
+     * Map that contains generators.
+     */
+    private final Map<String, Generator> map;
+
+    /**
+     * Constructor.
+     */
+    public Expressions() {
+        this.map = Collections.unmodifiableMap(this.init());
+    }
+
+    /**
+     * Generates source code from expression.
+     * @param expr The expression
+     * @return Java source code
+     */
+    public String generate(final Expression expr) {
+        return this.map.get(expr.getTypeName()).generate(expr);
+    }
+
+    /**
+     * Initialises map with generators.
+     * @return A map
+     */
+    private Map<String, Generator> init() {
+        final Map<String, Generator> gen = new TreeMap<>();
+        gen.put("IntegerLiteral", Node::getData);
+        gen.put(
+            "Addition",
+            expr -> {
+                final Addition node = (Addition) expr;
+                final String left = this.generate(node.getLeft());
+                final String right = this.generate(node.getRight());
+                return String.format("%s + %s", left, right);
+            }
+        );
+        return gen;
+    }
+
+    /**
+     * Interface for generating Java source code for a specific expression.
+     *
+     * @since 0.1.2
+     */
+    private interface Generator {
+        /**
+         * Generates Java source code for a specific expression.
+         * @param expr An expression
+         * @return Java source code
+         */
+        String generate(Expression expr);
+    }
+}
---
+++ src/test/java/org/cqfn/uast/lang/java/gen/ExpressionsTest.java
@@ -0,0 +1,55 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Ivan Kniazkov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.cqfn.uast.lang.java.gen;
+
+import java.util.Arrays;
+import org.cqfn.astranaut.core.Builder;
+import org.cqfn.astranaut.core.Factory;
+import org.cqfn.astranaut.core.Node;
+import org.cqfn.uast.tree.green.Expression;
+import org.cqfn.uast.tree.java.JavaFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for the Java expression generator.
+ *
+ * @since 0.1.2
+ */
+class ExpressionsTest {
+    @Test
+    void testAddition() {
+        final Factory factory = JavaFactory.INSTANCE;
+        final Builder left = factory.createBuilder("IntegerLiteral");
+        left.setData("2");
+        final Builder right = factory.createBuilder("IntegerLiteral");
+        right.setData("3");
+        final Builder builder = factory.createBuilder("Addition");
+        builder.setChildrenList(Arrays.asList(left.createNode(), right.createNode()));
+        final Node addition = builder.createNode();
+        final Expressions generator = new Expressions();
+        final String code = generator.generate((Expression) addition);
+        Assertions.assertEquals("2 + 3", code);
+    }
+}
