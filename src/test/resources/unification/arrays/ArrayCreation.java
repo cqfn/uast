b3ba2edee1d0d2fd4ba53c542427195230495394
Polina Volkhontseva <volkhontseva.polina@gmail.com>
2022-10-25 17:59:15 +0300

    Added unification tests

---
+++ src/test/resources/unification/arrays/ArrayCreation.java
@@ -0,0 +1,5 @@
+class Arrays {
+    public void create() {
+        int[] arr = {10, 20, 30, 40};
+    }
+}
---
+++ src/test/resources/unification/variable_declaration/ObjectDeclarator.java
@@ -0,0 +1,7 @@
+public class Code {
+    public static void main(String[] args) {
+        Object obj = new Object();
+        MyObject objarg = new MyObject(1, "Data");
+        Object empty = null;
+    }
+}
