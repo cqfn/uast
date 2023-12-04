5e76609e02c16f09a00094a89690b83be0858f93
Ivan Kniazkov <ivan@kniazkov.com>
2023-12-04 13:24:43 +0300

    Added test covering "if" statement in Java

---
+++ src/test/resources/unification/if_stmt/IfStmt.java
@@ -0,0 +1,11 @@
+public class Example {
+    public int sign(int x) {
+        if (x > 0) {
+            return 1;
+        }
+        if (x < 0) {
+            return 0;
+        }
+        return 0;
+    }
+}
